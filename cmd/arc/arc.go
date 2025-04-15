c68c89676903e3c1503488522ec9f28c29eabcc3
Andrew Suprun <andrew.suprun@gmail.com>
2025-04-15 02:13:57 -0400

    create root folders in needed

--- cmd/arc/arc.go
+++ cmd/arc/arc.go
@@ -23,6 +23,11 @@ func main() {
 	} else {
 		paths = make([]string, len(os.Args)-1)
 		for i, path := range os.Args[1:] {
+			err := os.MkdirAll(path, 0755)
+			if err != nil {
+				log.Debug("Failed to scan archives", "error", err)
+				panic(err)
+			}
 			path, err := filesys.AbsPath(path)
 			paths[i] = path
 			if err != nil {
